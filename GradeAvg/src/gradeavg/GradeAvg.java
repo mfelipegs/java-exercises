fa00376d69c531b891e83e060fe4f60c84d6584d
mfelipegs <matheusg.dasilva7@gmail.com>
2022-08-18 19:12:14 -0300

    Project files

--- GradeAvg/src/gradeavg/GradeAvg.java
+++ GradeAvg/src/gradeavg/GradeAvg.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gradeavg;
+
+/**
+ *
+ * @author fatec_aluno
+ */
 import java.util.Scanner;
 public class GradeAvg {
 	public static void main(String[] args) {
--- Salary/src/salary/Salary.java
+++ Salary/src/salary/Salary.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salary;
+
+/**
+ *
+ * @author fatec_aluno
+ */
 import java.util.Scanner;
 public class Salary {
 	public static void main(String[] args) {
