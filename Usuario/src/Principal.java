cefca18107c7b14f64e77b2b5b745527c41a417c
mfelipegs <matheusg.dasilva7@gmail.com>
2022-08-18 22:31:07 -0300

    a

---
+++ Usuario/src/Principal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author fatec_aluno
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //INSTANCIAÇÃO DO OBJETO (CRIAR O OBJETO)
+        //Classe=Usuario Objeto=user
+        Usuario user = new Usuario();
+        
+        //Setando os dados nos atributos (SETTER)
+        user.setNome("Matheus Felipe");
+        user.setLogin("math");
+        user.setSenha("12345Ma");
+    }
+    
+}
---
+++ Usuario/src/Usuario.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author fatec_aluno
+ */
+public class Usuario {
+    //DECLARAR OS ATRIBUTOS
+    private String nome;
+    private String login;
+    private String senha;
+    
+    //Construir os atributos para serem visualizados por outras classes
+    //CRIANDO CONSTRUTOR E INICIALIZANDO OS ATRIBUTOS VAZIOS
+    public Usuario() {
+        //inicie com valor vazio, se fosse int seria 0
+        this("","","");
+    }
+
+    //encapsulamento
+    public Usuario(String nome, String login, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    //podia ser this.nome ali, pq ta na propria classe
+    public void Mensagem() {
+        System.out.println("Nome: " + getNome()) + 
+                "\n Login: " + getLogin() + "\n Senha: " + getSenha() );
+    }
+}
