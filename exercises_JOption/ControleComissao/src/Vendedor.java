f540a63da8db5d2ba7aa7128967d5fa8173c3564
mfelipegs <matheusg.dasilva7@gmail.com>
2022-09-15 21:14:46 -0300

    init Vendedor

---
+++ exercises_JOption/ControleComissao/src/Vendedor.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author fatec_aluno
+ */
+public class Vendedor {
+    private String nome;
+    private double salarioBase;
+    private double valorVendido;
+    private double novoSalario;
+
+    public Vendedor() {
+        this("", 0.0, 0.0, 0.0);
+    }
+
+    public Vendedor(String nome, double salarioBase, double valorVendido, double novoSalario) {
+        this.nome = nome;
+        this.salarioBase = salarioBase;
+        this.valorVendido = valorVendido;
+        this.novoSalario = novoSalario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public double getValorVendido() {
+        return valorVendido;
+    }
+
+    public void setValorVendido(double valorVendido) {
+        this.valorVendido = valorVendido;
+    }
+
+    public double getNovoSalario() {
+        return novoSalario;
+    }
+
+    public void setNovoSalario(double novoSalario) {
+        this.novoSalario = novoSalario;
+    }
+    
+    public double calculoComissao(double valorVendido) {
+        setNovoSalario(((valorVendido*10)/100) + getSalarioBase()); 
+        return getNovoSalario();
+    }
+    
+    public void entrarNome() {
+        
+    }
+    
+    public void entrarSalarioBase() {
+        
+    }
+    
+    
+}
